a5b1a6b94ec4434c2b2a4a7614c85c099a59eeb3
Juuso Elo-Rauta <juuso.er@gmail.com>
2023-12-29 01:10:53 +0200

    first day done... the trie approach was way too complex. kiss

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,5 @@
 use crate::{Solution, SolutionPair};
-use std::{collections::HashMap, env, fs::read_to_string};
+use std::{env, fs::read_to_string};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -9,7 +9,7 @@ pub fn solve() -> SolutionPair {
     println!("path: {}", full_path.to_string_lossy());
     let input = read_to_string(full_path).unwrap();
     let sol1: u32 = p1(&input);
-    let sol2: u32 = 0;
+    let sol2: u32 = p2(&input).iter().sum();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
@@ -27,120 +27,44 @@ fn p1(data: &str) -> u32 {
                 .chars()
                 .filter_map(|c| c.to_digit(10))
                 .collect::<Vec<u32>>();
-            return format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap())
-                .parse::<u32>()
-                .ok();
+            return Some(numbers.first().unwrap() * 10 + numbers.last().unwrap());
         })
         .sum();
 }
 
-#[derive(Default, Debug)]
-struct TrieNode {
-    children: HashMap<char, TrieNode>,
-    value: u32,
-    is_leaf: bool,
-}
+const HR_DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-pub struct Trie {
-    root: TrieNode,
-}
+fn p2(data: &str) -> Vec<u32> {
+    let res = data.split("\n").filter(|l| !l.is_empty()).map(|line| {
+         let parsed = line.char_indices().filter_map(|(ci, char)| {
+            if let Some(val) = char.to_digit(10) {
+                return Some(val);
+            }
 
-impl Trie {
-    pub fn new() -> Self {
-        Trie {
-            root: TrieNode::default(),
-        }
-    }
+            for (wi, word) in (1u32..).zip(HR_DIGITS.iter()) {
+                if line[ci..].starts_with(word) {
+                    return Some(wi);
+                }
+            }
+            None
+        }).collect::<Vec<u32>>();
 
-    pub fn insert(&mut self, word: &str) {
-        let mut node = &mut self.root;
-        for char in word.chars() {
-            node = node.children.entry(char).or_insert(TrieNode::default());
-        }
-        node.is_leaf = true;
-        node.value = get_str_as_int(word)
-    }
-}
+         println!("Line: {} - Parsed {:?}", line, parsed);
+        return parsed.first().unwrap() * 10 + parsed.last().unwrap();
+    });
 
-fn p2(data: &str) -> Vec<Vec<u32>> {
-        let mut trie = Trie::new();
-        for word in vec![
-            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        ] {
-            trie.insert(word)
-        }
-    
-        return data.split("\n").
-            filter(|l| !l.is_empty())
-            .filter_map(|line| {
-                let root_children = &trie.root.children;
-                let mut node = &trie.root;
-                let mut last_char = 'x';
-                println!("Taking in line: {}", line);
-                let res = line.chars().filter_map(|char| {
-                    print!("{} -> ", char);
-                    if let Some(num) = char.to_digit(10) {
-                        // char is already a number so we can return and reset the search string
-                        node = &trie.root;
-                        println!("Is a number! Resetting node to root");
-                        return Some(num);
-                    }
-
-                    // char is _not_ a number so we need to start traversing the trie
-                    if let Some(next_node) = node.children.get(&char) {
-                        // found the char from trie, so we'll use that in the next round
-                        print!("found next node: ");
-                        node = next_node;
-                        last_char = char;
-                        return if node.is_leaf {
-                            // The node we found is a leaf node and thus should have a value
-                            // assosiated with it
-                            node = &trie.root;
-                            println!("Found a match! {}. Resetting to root node", node.value);
-                            Some(node.value)
-                        } else {
-                            None
-                        }
-                    }  else {
-                        // Char was not a direct child of the current node
-                        println!("No match in node for {}", char);
-                        node = match root_children.get(&last_char) {
-                            // Last letter was the beginning of a new word
-                            // This should take care of eightwo situtation...
-                            Some(found_node) => found_node.children.get(&char).or(Some(&trie.root)).unwrap(),
-                            None => &trie.root
-                        };
-                    }
-                    return None;
-                }).collect::<Vec<u32>>();
-            println!("Constructed {:?}", res);
-            Some(res)
-        }).collect();
-}
-
-fn get_str_as_int(digit: &str) -> u32 {
-    return match digit {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        &_ => 0,
-    };
+    return res.collect();
 }
 
 #[cfg(test)]
 mod tests {
     use super::p1;
     use super::p2;
-    use super::Trie;
 
     #[test]
-    fn p1Test() {
+    fn p1_test() {
         let test_input = "1abc2
             pqr3stu8vwx
             a1b2c3d4e5f
@@ -150,7 +74,7 @@ mod tests {
     }
 
     #[test]
-    fn p2Test() {
+    fn p2_test() {
         let test_input = "two1nine
 eightwothree
 abcone2threexyz
@@ -159,33 +83,8 @@ xtwone3four
 zoneight234
 7pqrstsixteen";
 
-        //let resVec: Vec<Vec<u32>> = p2(test_input);
-
-        //assert_eq!(resVec, vec![29, 83, 13, 24, 42, 14, 76]);
-        //assert_eq!(resVec.iter().sum::<u32>(), 281)
-    }
-
-    #[test]
-    fn p2Test2() {
-        let test_input = "1eightwo";
-        let res: Vec<Vec<u32>> = p2(test_input);
-
-        //assert_eq!(res, vec![vec![1,2]])
-    }
-
-    #[test]
-    fn trieTest() {
-        let mut trie = Trie::new();
-        for word in vec![
-            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        ] {
-            trie.insert(word)
-        }
-
-        match trie.root.children.get(&'n') {
-            Some(v) => println!("FOUND 'n' {:?}", v ),
-            None => println!("Trie not working")
-        };
-
+        let test = p2(test_input);
+        assert_eq!(test, vec![29, 83, 13, 24, 42, 14, 76]);
+        assert_eq!(test.iter().sum::<u32>(), 281)
     }
 }
